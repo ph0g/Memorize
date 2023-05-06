5b4da2a3628de6441aa7d5e917d1eb2cc6276699
Pierre-Hugues Oger <phylog@gmail.com>
2023-05-06 18:12:02 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Pierre-Hugues Oger on 06/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
